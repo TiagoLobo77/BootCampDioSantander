a3c16383cde86c26f291d4bac5e5b05c9821f8a9
TiagoLobo77 <tiago_batista77@outlook.com>
2022-07-27 22:29:01 -0300

    terceiro Commit - desafio Angular

---
+++ Como criar um front-end de um e-commerce utilizando Angular/bookstore-front/src/app/components/bookstore-app/bookstore-app.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookstore-app',
+  templateUrl: './bookstore-app.component.html',
+  styleUrls: ['./bookstore-app.component.css'],
+})
+export class BookstoreAppComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
